425130e83942fb6905fccc70cc5d9e228f507a56
Florian <florianjeremias@gmail.com>
2020-09-02 01:31:05 +0200

    added Flow Diagram for Script run

---
+++ exam.pl
@@ -0,0 +1,16 @@
+use v5.28.1;
+
+
+=pod
+=head1 Heading Text
+=head2 Heading Text
+=head3 Heading Text
+=head4 Heading Text
+=over indentlevel
+=item stuff
+=back
+=begin format
+=end format
+=for format text...
+=encoding type
+=cut
